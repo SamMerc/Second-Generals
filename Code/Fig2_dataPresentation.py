d4e0861c03124b36315529462e2e1e8766dc9ec9
SamMerc <samson.mercier18@gmail.com>
2026-04-18 19:55:24 -0400

    2026-04-18 - Update figure 2 plotting code to show density

--- Code/Fig2_dataPresentation.py
+++ Code/Fig2_dataPresentation.py
@@ -65,80 +65,66 @@ INPUT_LABELS = [
 
 FS = 12
 
-# 1. Define a 4-column mosaic: 
-# Col 0: Plot A | Col 1: The Gap between A&C | Col 2: Plot C | Col 3: Colorbars
-# 'G' is a dummy axis for the gap that we won't draw on.
+# Updated mosaic: 'Top' is now one single wide plot spanning the width of A+G+C
 fig, axes = plt.subplot_mosaic(
-    [['A', 'G', 'C', 'cbar_top'],
+    [['Top', 'Top', 'Top', 'cbar_top'],
      ['B', 'B', 'B', 'cbar_bot']], 
     figsize=(12, 8),
     gridspec_kw={
-        # Widths: [A, gap_width, C, cbar_width]
         'width_ratios': [1, 0.15, 1, 0.04], 
-        # wspace is now the gap between C and the colorbar. 
-        # Making this tiny pulls the colorbar in.
         'wspace': 0.02,
         'hspace': 0.1
     }
 )
 
-# Hide the dummy gap axis
-axes['G'].axis('off')
+# --- Top Plot: T-P Density Plot ---
+ax_tp = axes['Top']
 
-# --- Shared Colormap setup ---
-norm = mcolors.Normalize(vmin=min(raw_inputs[:, 0]), vmax=max(raw_inputs[:, 0]))
-cmap = cm.get_cmap('coolwarm')
-sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-sm.set_array([])
+# Flatten all data to get every single T-P coordinate pair in the dataset
+# raw_outputs_T and raw_outputs_P share the same shape
+all_T = raw_outputs_T.flatten()
+all_P = raw_outputs_P.flatten()
 
-# --- T profiles (Plot A) ---
-ax = axes['A']
-for i, raw_output_T in enumerate(raw_outputs_T):
-    ax.plot(raw_output_T, color=cmap(norm(raw_inputs[i, 0])))
-ax.xaxis.tick_top()
-ax.xaxis.set_label_position('top')
-ax.set_xlabel('Index', fontsize=FS)
-ax.set_ylabel('Temperature (K)', fontsize=FS)
-ax.tick_params(axis='both', labelsize=FS)
+# Use hexbin to calculate density - 'plasma' colormap as requested
+# gridsize: higher means more detail; mincnt=1 hides empty cells
+hb = ax_tp.hexbin(all_P, all_T, gridsize=100, cmap='plasma', mincnt=1, bins='log')
 
-# --- P profiles (Plot C) ---
-ax = axes['C']
-for i, raw_output_P in enumerate(raw_outputs_P):
-    ax.plot(raw_output_P, color=cmap(norm(raw_inputs[i, 0])))
-ax.xaxis.tick_top()
-ax.xaxis.set_label_position('top')
-ax.set_xlabel('Index', fontsize=FS)
-ax.set_ylabel(r'log$_{10}$ Pressure (bar)', fontsize=FS)
-ax.invert_yaxis()
-ax.tick_params(axis='both', labelsize=FS)
+# Formatting the T-P plot
+ax_tp.set_xlabel(r'log$_{10}$ Pressure (bar)', fontsize=FS)
+ax_tp.set_ylabel('Temperature (K)', fontsize=FS)
 
-# --- Top Colorbar ---
-cbar_t = fig.colorbar(sm, cax=axes['cbar_top'])
-cbar_t.set_label(INPUT_LABELS[0], fontsize=FS)
+# Move X-axis to top
+ax_tp.xaxis.tick_top()
+ax_tp.xaxis.set_label_position('top')
+
+# Invert Y-axis so Pressure increases downwards
+ax_tp.invert_xaxis()
+ax_tp.tick_params(axis='both', labelsize=FS)
+
+# # --- Top Colorbar (Density) ---
+cbar_t = fig.colorbar(hb, cax=axes['cbar_top'])
+cbar_t.set_label('Point Density (log$_{10}$ count)', fontsize=FS)
 cbar_t.ax.tick_params(labelsize=FS)
 
 # --- ST Map (Plot B) ---
-# Note: In the mosaic, B spans 3 columns to match the length of A + Gap + C
-ax = axes['B']
+ax_st = axes['B']
 hm = sns.heatmap(
-    raw_outputs_ST[2, :].reshape((IMG_H, IMG_W)), 
-    ax=ax, 
+    raw_outputs_ST[4, :].reshape((IMG_H, IMG_W)), 
+    ax=ax_st, 
     cbar_ax=axes['cbar_bot']
 )
-ax.set_ylabel('Latitude (deg)', fontsize=FS)
-ax.set_xlabel('Longitude (deg)', fontsize=FS)
-ax.set_yticks(np.linspace(0, IMG_H, 5))
-ax.set_yticklabels(np.linspace(-90, 90, 5).astype(int), fontsize=FS)
-ax.set_xticks(np.linspace(0, IMG_W, 5))
-ax.set_xticklabels(np.linspace(-180, 180, 5).astype(int), fontsize=FS)
+ax_st.set_ylabel('Latitude (deg)', fontsize=FS)
+ax_st.set_xlabel('Longitude (deg)', fontsize=FS)
+
+ax_st.set_yticks(np.linspace(0, IMG_H, 5))
+ax_st.set_yticklabels(np.linspace(-90, 90, 5).astype(int), fontsize=FS)
+ax_st.set_xticks(np.linspace(0, IMG_W, 5))
+ax_st.set_xticklabels(np.linspace(-180, 180, 5).astype(int), fontsize=FS)
 
 # --- Bottom Colorbar ---
 axes['cbar_bot'].set_ylabel('Temperature (K)', fontsize=FS)
 axes['cbar_bot'].tick_params(labelsize=FS)
 
-# Final adjustment
-# We use constrained_layout or tight_layout, but since we have a custom gap, 
-# tight_layout might try to "fix" it. We'll use subplots_adjust for the final polish.
-plt.subplots_adjust(top=0.90, bottom=0.1, left=0.08, right=0.92)
+plt.subplots_adjust(top=0.88, bottom=0.1, left=0.1, right=0.92)
 
-plt.show()
+plt.savefig(plot_save_path+'Fig2_density.pdf')
